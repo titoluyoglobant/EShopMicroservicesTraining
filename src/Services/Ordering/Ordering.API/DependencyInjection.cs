3ed72cc1d22c09c1f47f5c85ff80602d8eca0652
Tito Luyo Murata <tluyo@hotmail.com>
2024-03-19 12:33:23 -0500

    domain, DI

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        //services.AddCarter();
+
+        return services;
+    }
+
+    public static WebApplication UserApiServices(this WebApplication app)
+    {
+        //app.MapCarter();
+
+        return app;
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,27 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+
+//----------------
+//Infrastructure - EF Core
+//Application - MediatR
+//API - Carter, HealthChecks, ...
+
+//builder.Services
+//    .AddApplicationServices()
+//    AddInfrastructureServices(builder.Configuration)
+//    AddWebServices();
+//---------------
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 
 var app = builder.Build();
 
