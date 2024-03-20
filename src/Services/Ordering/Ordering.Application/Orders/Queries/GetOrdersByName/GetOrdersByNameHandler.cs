6c0d15f724753e44fc778d86a0dea8a34cddfdbd
Tito Luyo Murata <tluyo@hotmail.com>
2024-03-19 23:45:30 -0500

    orders extension

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+﻿using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
 
 public class GetOrdersByNameHandler
     (IApplicationDbContext dbContext)
@@ -16,56 +18,6 @@ public class GetOrdersByNameHandler
             .OrderBy(o => o.OrderName)
             .ToListAsync(cancellationToken);
 
-        var orderDtos = ProjectToOrderDto(orders);
-
-        return new GetOrdersByNameResult(orderDtos);
-    }
-
-    private List<OrderDto> ProjectToOrderDto(List<Order> orders)
-    {
-        List<OrderDto> result = new();
-        foreach (var order in orders)
-        {
-            var orderDto = new OrderDto(
-                Id: order.Id.Value,
-                CustomerId: order.CustomerId.Value,
-                OrderName: order.OrderName.Value,
-                ShippingAddress: new AddressDto(
-                    FirstName: order.ShippingAddress.FirstName,
-                    LastName: order.ShippingAddress.LastName,
-                    EmailAddress: order.ShippingAddress.EmailAddress,
-                    AddressLine: order.ShippingAddress.AddressLine,
-                    Country: order.ShippingAddress.Country,
-                    State: order.ShippingAddress.State,
-                    ZipCode: order.ShippingAddress.ZipCode
-                ),
-                BillingAddress: new AddressDto(
-                    FirstName: order.BillingAddress.FirstName,
-                    LastName: order.BillingAddress.LastName,
-                    EmailAddress: order.BillingAddress.EmailAddress,
-                    AddressLine: order.BillingAddress.AddressLine,
-                    Country: order.BillingAddress.Country,
-                    State: order.BillingAddress.State,
-                    ZipCode: order.BillingAddress.ZipCode
-                ),
-                Payment: new PaymentDto(
-                    CardName: order.Payment.CardName,
-                    CardNumber: order.Payment.CardNumber,
-                    Expiration: order.Payment.Expiration,
-                    Cvv: order.Payment.CVV,
-                    PaymentMethod: order.Payment.PaymentMethod
-                ),
-                Status: order.Status,
-                OrderItems: order.OrderItems.Select(item => new OrderItemDto(
-                    OrderId: item.OrderId.Value,
-                    ProductId: item.ProductId.Value,
-                    Quantity: item.Quantity,
-                    Price: item.Price
-                )).ToList()
-            );
-            result.Add(orderDto);
-        }
-
-        return result;
+        return new GetOrdersByNameResult(orders.ToOrderDtoList());
     }
 }
