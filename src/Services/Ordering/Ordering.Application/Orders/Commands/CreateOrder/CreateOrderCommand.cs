2bcd93698118601adf709ade2af98d80fa2a61b6
Tito Luyo Murata <tluyo@hotmail.com>
2024-03-19 22:32:58 -0500

    create order command handler, IAppDbCtx

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order)
+    : ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(o => o.Order.OrderName).NotEmpty().WithMessage("Name is required.");
+        RuleFor(o => o.Order.CustomerId).NotEmpty().WithMessage("CustomerId is required.");
+        RuleFor(o => o.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be empty.");
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
-using Ordering.Domain.Models;
+using Ordering.Application.Data;
 
 namespace Ordering.Infrastructure.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
