f0b08fcf38bb20d54ffa7cf5345859c5306786d8
Tito Luyo Murata <tluyo@hotmail.com>
2024-03-19 22:57:42 -0500

    app commands

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Orders.Commands.UpdateOrder;
+
+public record UpdateOrderCommand(OrderDto Order)
+    : ICommand<UpdateOrderResult>;
+
+public record UpdateOrderResult(bool IsSuccess);
+
+public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+{
+    public UpdateOrderCommandValidator()
+    {
+        RuleFor(o => o.Order.Id).NotEmpty().WithMessage("Id is required.");
+        RuleFor(o => o.Order.OrderName).NotEmpty().WithMessage("Name is required.");
+        RuleFor(o => o.Order.CustomerId).NotNull().WithMessage("CustomerId is required.");
+    }
+}
